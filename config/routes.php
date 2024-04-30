51135217ab8defd85262886b03f9f270ea1310b4
Yakov Levin <117316056+Izyus@users.noreply.github.com>
2024-04-30 22:11:11 +0300

    first commit

---
+++ config/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Router\Route;
+
+
+return [
+    '/home' => function () {
+    echo 'zxcxcz';
+    }
+];
+
+
+
+
+//return [
+//    Route::get('/projects', function () {
+//        include_once APP_PATH . '/views/pages/projects.php';
+//    }),
+//    Route::get('/about', function () {
+//        include_once APP_PATH . '/views/pages/about.php';
+//    })
+//];
